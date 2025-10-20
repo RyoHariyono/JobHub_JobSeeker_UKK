537d5b2467c51e512178668ffc5159f496ad2ad9
lordbruh0 <ryohariyono17@gmail.com>
2025-10-20 11:20:25 +0700

    feat: :zap: Creating navigation notification
    Creating notfication page navigation

--- lib/app/modules/notification/notification_paga.dart
+++ lib/app/modules/notification/notification_paga.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:jobhub_jobseeker_ukk/core/theme/app_color.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class NotificationPaga extends StatelessWidget {
   const NotificationPaga({super.key});
@@ -6,7 +9,29 @@ class NotificationPaga extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Notification Page')),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        toolbarHeight: 70,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            LucideIcons.chevronLeft,
+            color: AppColors.darkGrey,
+            size: 24,
+          ),
+          onPressed: () => context.go('/'),
+        ),
+        title: Text(
+          "Notification",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: AppColors.darkGrey,
+          ),
+        ),
+        centerTitle: true,
+      ),
       body: const Center(child: Text('Welcome to the Notification Page!')),
     );
   }
